3b498986cb20db532162340809b390da8255a576
Nixerrr <nikoszp@gmail.com>
2017-12-04 19:07:48 +0100

    fix: flask-mail bug

--- application.py
+++ application.py
@@ -7,13 +7,13 @@ from checkfordupes import *
 
 # configure application
 app = Flask(__name__)
-mail=Mail(app)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.environ['GMAIL_ADDRESS']  # you need to provide this
 app.config['MAIL_PASSWORD'] = os.environ['GMAIL_PASSWORD']  # you need to provide this
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+mail=Mail(app)
 
 # the app itself
 @app.route('/', methods=["GET", "POST"])
